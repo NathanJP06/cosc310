1789a5c0a6a27686eedeaacb84570a107b6d0234
NathanP06 <nathanjpugh6@gmail.com>
2026-01-28 11:46:40 -0600

    ListAppend method

--- chapter6/ArrayAppend.java
+++ chapter6/ArrayAppend.java
@@ -17,6 +17,7 @@ public class ArrayAppend extends Target {
             result += arr[indiciesOrNums[i]];
             largerArr[arr.length + i] = indiciesOrNums[i];
         }
+        
         return result;
     }
     
--- chapter6/ListAppend.java
+++ chapter6/ListAppend.java
@@ -10,6 +10,12 @@ public class ListAppend extends Target{
     @Override
     public int method(int indiciesOrNums[]){
         int result = 0;
+
+        for(int i = 0; i < indiciesOrNums.length; i++){
+            result += list.get(indiciesOrNums[i]);
+            list.add(indiciesOrNums[i]);
+        }
+        
         return result;
     }
 
